b1eb38600a20ec7dcfa0ce38b7991123b73667a5
Kamran Ansari <ranmerc@outlook.com>
2026-07-08 21:54:59 +0530

    chore: add oxc

--- src/edges/index.ts
+++ src/edges/index.ts
@@ -1,9 +1,9 @@
-import type { Edge, EdgeTypes } from '@xyflow/react';
+import type { Edge, EdgeTypes } from "@xyflow/react";
 
 export const initialEdges: Edge[] = [
-  { id: 'a->c', source: 'a', target: 'c', animated: true },
-  { id: 'b->d', source: 'b', target: 'd' },
-  { id: 'c->d', source: 'c', target: 'd', animated: true },
+  { id: "a->c", source: "a", target: "c", animated: true },
+  { id: "b->d", source: "b", target: "d" },
+  { id: "c->d", source: "c", target: "d", animated: true },
 ];
 
 export const edgeTypes = {
